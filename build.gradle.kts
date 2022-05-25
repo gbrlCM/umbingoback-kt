57251626646aeff9fa064f54b90062b5727d6831
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2022-05-25 20:09:34 -0300

    changed java compac version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 }
 
